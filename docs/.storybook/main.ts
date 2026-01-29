28c4d1d0dcd0262b740f342588cc0a9dfda09ed9
wongil12 <judgevi52@gmail.com>
2026-01-29 18:27:54 +0900

    feat: initial storybook & add color to storybook

---
+++ docs/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  docs: {},
+};
+
+export default config;
