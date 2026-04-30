7b9bb17fed0dfcbcb9541b6c50063790bd9a1a80
Hyunkwan Sung <gwansammy@gmail.com>
2026-04-30 11:57:46 +0900

    fix: fix tailwind variable issue

--- dummy/TextField/types.tsx
+++
@@ -1,5 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-
-import { FormGroupProps } from '@@components/FormGroup/types';
-
-export type TextFieldProps = InputHTMLAttributes<HTMLInputElement> & FormGroupProps;
